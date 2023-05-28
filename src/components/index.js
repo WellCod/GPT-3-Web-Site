025ad2e5bcb225dd606880f67d4fabfe9452b7e1
WellCod <weslleygoncalves18@gmail.com>
2023-05-28 17:11:54 -0300

    Versão final do website GPT-3

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from './article/Article';
+export { default as Brand } from './article/brand/Brand';
+export { default as CTA } from './article/cta/CTA';
+export { default as Feature } from './article/feature/Feature';
+export { default as Navbar } from './article/navbar/Navbar';
